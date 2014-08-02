39ac10332cee63d8de44e9cb80b0b5b306ba183e
Arnold <sh8erby@knights.ucf.edu>
2014-08-02 14:18:26 -0400

    Initial Commit
    Initial Commit

---
+++ Angular.Template/Content/app/app.js
@@ -0,0 +1,13 @@
+﻿/*
+  This is the main app module definition. It binds to the entire page using the ng-app directive. 
+  See Shell.cshtml and look at the HTML root node
+*/
+angular.module('ngscaffold', ['ngRoute', 'ngscaffold.providers'
+                              ,'ngscaffold.controllers','ngscaffold.services','ngscaffold.directives'])
+       .config(function ($routeProvider,
+                         $httpProvider,
+                         $appRouterProvider) {
+
+                    var route = $appRouterProvider.route;
+                    $routeProvider.when('/', route('home'));
+                });
